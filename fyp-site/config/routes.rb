547ed8272cd775a10a9a6a6a0b0d3cb50fd460bd
itscholey <chloebarnes3@gmail.com>
2017-02-03 18:15:23 +0000

    User creation form added

--- fyp-site/config/routes.rb
+++ fyp-site/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'users#new'
+  resources :users
+  root 'users#index'
+
 
   # This line mounts Refinery's routes at the root of your application.
   # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
