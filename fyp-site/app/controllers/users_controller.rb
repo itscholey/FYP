b0be05bd93f1b33232dee8a4f8825cec5e76da1a
itscholey <chloebarnes3@gmail.com>
2017-02-07 16:05:58 +0000

    Added layouts

--- fyp-site/app/controllers/users_controller.rb
+++ fyp-site/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.all
   end
@@ -25,4 +27,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:username, :email, :name,
                                     :password, :password_confirmation)
     end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in to continue."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
 end
