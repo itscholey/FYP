5dc0701b465df1706908eacc47cfe23492bb8351
itscholey <chloebarnes3@gmail.com>
2017-03-03 16:19:59 +0000

    Formatted messages.

--- fyp/fyp-site/app/controllers/application_controller.rb
+++ fyp/fyp-site/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  include MessagesHelper
 
   def redirect_back_or_root
     if request.env["HTTP_REFERER"].present?
